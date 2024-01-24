4c329b2ccbcf41ee0c3765134429dc1e526b3aea
serena-peschiaroli <serena.peschiaroli@gmail.com>
2024-01-24 17:14:27 +0100

    Add: soft_delete table; model

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -5,10 +5,12 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Str;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
 
 class Project extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
     protected $fillable= ['title', 'description', 'slug', 'repository_url' ];
     //mutaator
     public function setTitleAttribute($_title){
