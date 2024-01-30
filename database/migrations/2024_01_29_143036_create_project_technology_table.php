e921fb0861b92bd62edabdd1b292f2206fec4658
serena-peschiaroli <serena.peschiaroli@gmail.com>
2024-01-30 10:17:31 +0100

    Fix: typo in project technology table

--- database/migrations/2024_01_29_143036_create_project_technology_table.php
+++ database/migrations/2024_01_29_143036_create_project_technology_table.php
@@ -14,11 +14,9 @@ return new class extends Migration
     public function up()
     {
         Schema::create('project_technology', function (Blueprint $table) {
-            $table->id();
+           
             $table->unsignedBigInteger('project_id');
             $table->unsignedBigInteger('technology_id');
-            $table->timestamps();
-
             $table->foreign('project_id')->references('id')->on('projects')->onDelete('cascade');
             $table->foreign('technology_id')->references('id')->on('technologies')->onDelete('cascade');
             $table->unique(['project_id', 'technology_id']);
